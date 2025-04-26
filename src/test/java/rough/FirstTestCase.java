a539d52bab9f697853e5f7cddf750fd36c8259c5
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2025-04-26 19:58:51 +0530

    Updated Folder Structure

--- src/test/java/rough/FirstTestCase.java
+++ src/test/java/rough/FirstTestCase.java
@@ -6,7 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTestCase {
 
-	public static void main(String[] args) throws Exception {
+	public static void sleepUsingThread(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) {
 		// Launch Chrome Browser
 		WebDriver driver = new ChromeDriver();
 		// Open the Webpage
@@ -17,10 +25,11 @@ public class FirstTestCase {
 		if (titleParts[0].equals("Gearsmith")) {
 			System.out.println("Passed");
 		}
-		Thread.sleep(5000);
+		FirstTestCase.sleepUsingThread(5);
 		driver.findElement(By.xpath("//span[text()='Log In']")).click();
-		Thread.sleep(3000);
+		FirstTestCase.sleepUsingThread(3);
 		driver.findElement(By.xpath("//span[text()='Sign up with email']")).click();
+		FirstTestCase.sleepUsingThread(5);
 		driver.quit();
 	}
 }
